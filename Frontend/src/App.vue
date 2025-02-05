8c661c9237ed45a87672331d3e55b4b415c25324
jip11 <jip463210@gmail.com>
2025-02-05 19:04:44 +0100

    Navbar létrehozva App.vueban

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+    <div id="navbar">
+      <div id="navcomponent" style="padding: 10px;">
+      <img src="#" alt="Logo">
+      <b>ConsoleX</b>
+      </div>
+      <div id="navcomponent" style="justify-content: flex-end;">
+        <div id="navbuttons">
+      <button>Home</button>
+      <button>About</button>
+      <button>Products</button>
+      <button>Contacts</button>
+        </div>
+      </div>
+
+    </div>
     <router-view></router-view> <!-- Itt jelennek meg a komponensek -->
   </div>
 </template>
@@ -11,5 +26,20 @@ export default {
 </script>
 
 <style>
-/* Alap stílusok */
+#navbar{
+  display: flex;
+  border-bottom: 2px black solid;
+  align-items: center;
+  height: 50px;
+}
+#navcomponent{
+  width: 50%;
+  display: flex;
+  gap: 10px;
+}
+
+#navbuttons{
+  text-align: right;
+}
+
 </style>
--- Frontend/src/components/LoginComponets.vue
+++ Frontend/src/components/LoginComponets.vue
@@ -1,22 +1,5 @@
 <template>
-  <v-container>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>My App</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text>Home</v-btn>
-      <v-btn text>Gaming</v-btn>
-      <v-btn text>Office</v-btn>
-      <v-btn text>Checkout</v-btn>
-      <v-btn text>Contacts</v-btn>
-      <v-text-field
-        dense
-        solo
-        placeholder="Search..."
-        prepend-inner-icon="mdi-magnify"
-        class="searchbar"
-      ></v-text-field>
-    </v-app-bar>
-  </v-container>
+
 </template>
 
 <script>
