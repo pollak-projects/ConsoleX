bdf516ba1a091389201210450ea29a9058b8f5c9
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2025-02-17 13:33:13 +0100

    update backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors'); // Ha a frontend másik porton fut, szükséges lehet CORS kezelés
 const app = express();
+const route = require('./routes/route')
+
+app.use('/api/order', route);
 
 // CORS beállítása, hogy a frontend kommunikálhasson a szerverrel
 app.use(cors());
@@ -30,6 +33,18 @@ db.connect((err) => {
 // API végpont a termékek lekéréséhez
 app.get('/api/product', (req, res) => {
   const sql = 'SELECT * FROM products'; // Feltételezve, hogy a tábla neve 'products'
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Hiba történt a lekérdezés során: ', err);
+      return res.status(500).json({ message: 'Hiba történt a lekérdezés során' });
+    }
+    res.json(results);
+  });
+});
+
+app.get('/api/order', (req, res) => {
+  const sql = 'SELECT * FROM order'; // Feltételezve, hogy a tábla neve 'products'
 
   db.query(sql, (err, results) => {
     if (err) {
