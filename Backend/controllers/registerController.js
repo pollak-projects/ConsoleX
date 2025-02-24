031591c85f4325a84a56d5d3492fea03686aa6e8
jip11 <58308465+jip11@users.noreply.github.com>
2025-02-24 09:39:07 +0100

    Username és Email azonnos megoldva

--- Backend/controllers/registerController.js
+++ Backend/controllers/registerController.js
@@ -3,19 +3,32 @@ const db = require('../config/db');
 
 exports.registerUser = async (req, res) => {
     const { username, email, password } = req.body;
-console.log(username, email, password);
+    console.log(username, email, password);
+
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const sqlQuery = 'INSERT INTO user (username, email, password) VALUES (?, ?, ?)';
-        db.query(sqlQuery, [username, email, hashedPassword], (err, result) => {
+        const checkQuery = 'SELECT * FROM user WHERE username = ? OR email = ?';
+        db.query(checkQuery, [username, email], async (err, results) => {
             if (err) {
-                console.error('Error during user registration:', err);
-                return res.status(500).json({ message: 'An error occurred during user registration' });
+                console.error('Error during user check:', err);
+                return res.status(500).json({ message: 'Egy hiba lépett fel a regisztráció során' });
             }
-            res.status(201).json({ message: 'User successfully registered', userId: result.insertId });
+
+            if (results.length > 0) {
+                return res.status(400).json({ message: 'A felhasználónév vagy az Email már használatban van!' });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const sqlQuery = 'INSERT INTO user (username, email, password) VALUES (?, ?, ?)';
+            db.query(sqlQuery, [username, email, hashedPassword], (err, result) => {
+                if (err) {
+                    console.error('Error during user registration:', err);
+                    return res.status(500).json({ message: 'Egy hiba lépett fel a regisztráció során' });
+                }
+                res.status(201).json({ message: 'Sikeresen regisztráltál!', userId: result.insertId });
+            });
         });
     } catch (err) {
         console.error('Error during password hashing:', err);
-        res.status(500).json({ message: 'An error occurred during user registration' });
+        res.status(500).json({ message: 'Egy hiba lépett fel a regisztráció során' });
     }
 };
