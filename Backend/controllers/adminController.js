8437969a189c0d8c06ec379c7af9ed21112a2e16
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2025-02-26 08:36:11 +0100

    update adminoldal

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -2,8 +2,8 @@ const db = require('../config/db');
 
 exports.addProduct = (req, res) => {
   const { name, price, image, category } = req.body;
-  const sql = 'INSERT INTO products (name, price, image, category) VALUES (?, ?, ?, ?)';
-  db.query(sql, [name, price, image, category], (err, result) => {
+  const query = 'INSERT INTO products (name, price, image, category) VALUES (?, ?, ?, ?)';
+  db.query(query, [name, price, image, category], (err, result) => {
     if (err) throw err;
     res.send('Product added successfully');
   });
