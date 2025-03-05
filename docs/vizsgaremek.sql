2a1adf1399959373bd1b4f1759462db26c5c0003
keskenykevin <130552879+keskenykevin@users.noreply.github.com>
2025-03-05 10:24:50 +0100

    sql

--- docs/vizsgaremek.sql
+++ docs/vizsgaremek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 25. 09:28
+-- Létrehozás ideje: 2025. Már 05. 10:23
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `adminuser`
---
-
-CREATE TABLE `adminuser` (
-  `adminid` int(255) NOT NULL,
-  `adminusername` text NOT NULL,
-  `adminpassword` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `adminuser`
---
-
-INSERT INTO `adminuser` (`adminid`, `adminusername`, `adminpassword`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
 -- Tábla szerkezet ehhez a táblához `category`
 --
 
@@ -58,11 +39,13 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `order` (
-  `order_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `order_date` date NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
-  `status` varchar(50) NOT NULL
+  `id` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `zip` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `houseNumber` varchar(10) NOT NULL,
+  `otherDetails` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -98,12 +81,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `name`, `price`, `image`, `category`) VALUES
-(1, 'Call of Duty: Black Ops 6', 25000.00, 'https://placehold.co/200x200', 'PC'),
-(2, 'Minecraft', 10000.00, 'https://placehold.co/200x200', 'Xbox'),
-(3, 'Minecraft', 10000.00, 'https://placehold.co/200x200', 'PC'),
-(4, 'Forza Horizon 4 (használt)', 15000.00, 'https://placehold.co/200x200', 'Használt Xbox'),
-(5, 'The Last of Us Part II', 15000.00, 'https://placehold.co/200x200', 'PlayStation'),
-(6, 'FIFA 22', 12000.00, 'https://placehold.co/200x200', 'Xbox');
+(1, 'Call Of Duty: Black Ops 6 (PlayStation 4)', 25000.00, 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_142344989?x=1800&y=1800&format=jpg&quality=80&sp=yes&strip=yes&trim&ex=1800&ey=1800&align=center&resizesource&unsharp=1.5x1+0.7+0.02&cox=0&coy=0&cdx=1800&cdy=1800', 'PlayStation'),
+(2, 'The Witcher 3: Wild Hunt Game of The Year Edition (GOTY)\r\nPC', 10000.00, 'https://content.konzolvilag.hu/product_instances2/189820/pc-the-witcher-3-wild-hunt-game-of-the-year-edition-goty_thumb674.jpg?lmod=1701862650', 'PC'),
+(3, 'Grand Theft Auto V (GTA 5)', 10000.00, 'https://content.konzolvilag.hu/product_instances2/985623/xboxseries-grand-theft-auto-v_thumb674.jpg?lmod=1651736766', 'Xbox'),
+(5, 'Far Cry 6', 15000.00, 'https://content.konzolvilag.hu/product_instances2/898651/xboxone-far-cry-6_thumb674.jpg?lmod=1687790568', 'Xbox'),
+(6, 'FC 25 (PlayStation)', 12000.00, 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_143289391?x=1800&y=1800&format=jpg&quality=80&sp=yes&strip=yes&trim&ex=1800&ey=1800&align=center&resizesource&unsharp=1.5x1+0.7+0.02&cox=0&coy=0&cdx=1800&cdy=1800', 'PlayStation'),
+(7, 'Hogwarts Legacy (PlayStation 5)', 15000.00, 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_100544660?x=1800&y=1800&format=jpg&quality=80&sp=yes&strip=yes&trim&ex=1800&ey=1800&align=center&resizesource&unsharp=1.5x1+0.7+0.02&cox=0&coy=0&cdx=1800&cdy=1800', 'PlayStation'),
+(9, 'NBA 2K25 (Xbox Series X & Xbox One)', 20000.00, 'https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_143979958?x=1800&y=1800&format=jpg&quality=80&sp=yes&strip=yes&trim&ex=1800&ey=1800&align=center&resizesource&unsharp=1.5x1+0.7+0.02&cox=0&coy=0&cdx=1800&cdy=1800', 'Xbox'),
+(10, 'Marvel\'s Spider-Man 2 (használt)', 10000.00, 'https://content.konzolvilag.hu/product_instances_cover/Konzolvilag_0/1034549/playstation5-marvels-spider-man-2_thumb674.jpg?lmod=1718291146', 'Használt PlayStation');
 
 -- --------------------------------------------------------
 
@@ -130,14 +115,18 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexek a kiírt táblákhoz
+-- A tábla adatainak kiíratása `user`
 --
 
+INSERT INTO `user` (`user_id`, `username`, `email`, `password`) VALUES
+(1, 'asd', 'asd@gmail.com', '$2b$10$uMvaTgU17DSKT3vNMcC6kuQUJ5B9.31jtWr3nydpWwBKcLkddYpA2'),
+(4, 'dsasadsda', 'sdasadsda@gmail.com', '$2b$10$ZuC9W81ITTSY1inv83mvzOsWmJULuvY/vDNJ4iXSunbGV3n55eiJ6'),
+(5, 'proba1', 'proba1@gmail.com', '$2b$10$fRBqtBFjG4d8QRh/B35HtOmYhjXsI7UWdA8hLqctUeOHFB4F75Pu.'),
+(6, 'valami', 'valami@gmail.com', '$2b$10$Zl81FAantc76BPxgtxDVn.dl8q/as5L/eAlu/KKjg2XkbeSfAn9xa');
+
 --
--- A tábla indexei `adminuser`
+-- Indexek a kiírt táblákhoz
 --
-ALTER TABLE `adminuser`
-  ADD PRIMARY KEY (`adminid`);
 
 --
 -- A tábla indexei `category`
@@ -149,8 +138,7 @@ ALTER TABLE `category`
 -- A tábla indexei `order`
 --
 ALTER TABLE `order`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- A tábla indexei `order_item`
@@ -185,12 +173,6 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT a táblához `adminuser`
---
-ALTER TABLE `adminuser`
-  MODIFY `adminid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT a táblához `category`
 --
 ALTER TABLE `category`
@@ -200,7 +182,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT a táblához `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `order_item`
@@ -212,23 +194,13 @@ ALTER TABLE `order_item`
 -- AUTO_INCREMENT a táblához `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT a táblához `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Megkötések a kiírt táblákhoz
---
-
---
--- Megkötések a táblához `order`
---
-ALTER TABLE `order`
-  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
