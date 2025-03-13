7e0dcbad2d39e1aa5f548aabf96962623749dce7
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2025-03-13 12:49:19 +0100

    update backend

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -11,16 +11,3 @@ exports.getAllProducts = (req, res) => {
     res.json({ products: results });
   });
 };
-
-exports.addProduct = (req, res) => {
-  const { name, category, price, image } = req.body;
-
-  const sqlQuery = 'INSERT INTO products (name, category, price, image) VALUES (?, ?, ?, ?)';
-  db.query(sqlQuery, [name, category, price, image], (err, result) => {
-    if (err) {
-      console.error('Hiba a termék hozzáadása során:', err);
-      return res.status(500).json({ message: 'Hiba történt a termék hozzáadása során' });
-    }
-    res.status(201).json({ message: 'Termék sikeresen hozzáadva', productId: result.insertId });
-  });
-};
