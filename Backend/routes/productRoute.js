8716faffca23222cc388870292b8bbb216efb257
dancsikdavid <130552807+dancsikdavid@users.noreply.github.com>
2025-03-17 08:23:24 +0100

    update backend

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/products', productController.getAllProducts);
-router.post('/products', productController.addProduct);
+
 
 module.exports = router;
